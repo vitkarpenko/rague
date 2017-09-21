c05343d280677aa4ff221f3a12d50cd79dfc22f3
Vitaly Karpenko <vitkarpenko@gmail.com>
2017-09-21 10:22:37 +0300

    Added paralyzed entity test.

--- tests/test_systems.py
+++ tests/test_systems.py
@@ -16,3 +16,13 @@ def test_movement_system_on_dummy_entity_two_turns(world, dummy_entity):
         == dummy_entity.components['Position'].y
         == 2
     )
+
+def test_movement_system_on_paralyzed_dummy(world, dummy_entity):
+    world.entities.add(dummy_entity)
+    del dummy_entity.components['Velocity']
+    world.make_iteration()
+    assert (
+        dummy_entity.components['Position'].x
+        == dummy_entity.components['Position'].y
+        == 0
+    )
