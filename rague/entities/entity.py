66032f2a722c3f535e0bf66b44cb7d8d68fe0b3e
Vitaly Karpenko <vitkarpenko@gmail.com>
2017-09-25 09:04:43 +0300

    Entity metaclass works.

---
+++ rague/entities/entity.py
@@ -0,0 +1,45 @@
+""" This module provides main Entity class
+used as a base for all other entities (through
+subclassing or instantiation.
+"""
+from rague.components import (
+    Component,
+    Position,
+    Velocity,
+    Player
+)
+
+
+class EntityMeta(type):
+    """ Every entity should declare a set of "components" instances
+    which are used as a default values for instantiation.
+    This metaclass turns this set into a dictionary
+    for convenience.
+    """
+    def __new__(cls, name, bases, attrs):
+        if 'components' not in attrs:
+            raise AttributeError('Entity subclasses should declare a set of '
+                                 'Components called "components".')
+        clsobj = super().__new__(cls, name, bases, attrs)
+        components_dict = {
+            component.__class__.__name__: component
+            for component in attrs['components']
+        }
+        attrs['components'] = components_dict
+        return clsobj
+
+
+class Entity(metaclass=EntityMeta):
+    components = {}
+
+    def __init__(self, *args):
+        init_components = {
+            component.__class__.__name__: component
+            for component in args
+            if isinstance(component, Component)
+        }
+        merged_components = {**self.components, **init_components}
+        if not merged_components:
+            return
+        for key, val in merged_components.items():
+            setattr(self, key.lower(), val)
